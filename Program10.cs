eda36d36d61d6f132bb784d74e20fa1ef130030d
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-08-07 14:36:07 +0300

    Add files via upload

--- Program10.cs
+++ Program10.cs
@@ -4,68 +4,59 @@ namespace PasswordValidator
 {
     internal class Program
     {
-        static void CharAmount(string password, bool valid)
+        static void PassCheck(string pass)
         {
-            char[] passwordCheck = password.ToArray();
-            if (passwordCheck.Length < 6 || passwordCheck.Length > 10)
+            char[] passwordArray = pass.ToArray();
+            bool check = true;
+            if(passwordArray.Length<6 || passwordArray.Length > 10)
             {
-                Console.WriteLine("Password must be between 6 and 10 characters.");
-
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                check = false;
             }
-            else
+
+
+            int c = 0;
+            for(int i = 0; i < passwordArray.Length; i++)
             {
-                valid = true;
-            }
-        }
 
-        static void CharContain(string password, bool valid)
-        {
-            
-        }
-        static void AtLeast2Digits(string password, bool valid)
-        {           
-            char[] passwordCheck = password.ToArray();
-            
+                if (char.IsLetter(passwordArray[i])==false && char.IsDigit(passwordArray[i])==false)
+                {                   
+                    c++;                   
+                }
+            }
+            if (c > 0)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+                check = false;
+            }
 
             int br = 0;
-            string s1 = "0";
-            string s2 = "9";
-            char[] char1 = s1.ToArray();
-            char[] char2 = s2.ToArray();
-
-            for(char i = char1[0]; i <= char2[0]; i++)
-            {
-                if (passwordCheck.Contains(i))
+            for (int i = 0; i < passwordArray.Length; i++)
+            {               
+                if(char.IsDigit(passwordArray[i]) == true)
                 {
                     br++;
                 }
             }
-
-            if (br < 2)
+            if(br < 2)
             {
                 Console.WriteLine("Password must have at least 2 digits");
-
+                check = false;
             }
 
-            else
+            if(check == true)
             {
-                valid = true;
-            } 
-        }
+                Console.WriteLine("Password is valid");
+            }
+        } 
+
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Password:");
             string password = Console.ReadLine();
-            bool valid = false;
 
-            CharAmount(password, valid);
-            CharContain(password, valid);
-            AtLeast2Digits(password, valid);
-
-            if (valid == true)
-            {
-                Console.WriteLine("Password is valid.");
-            }
+            PassCheck(password);
         }
     }
 }
